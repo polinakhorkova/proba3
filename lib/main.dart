8f2ed4e3cb6f2b5d724615234e62ce7f82b70fc4
polinakhorkova <polinakhorkova@mail.ru>
2024-05-28 22:44:29 +0500

    main init services

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:proba3/app/services/socket_service.dart';
+import 'package:proba3/app/services/user_service.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
+  await initServices(); 
   runApp(
     GetMaterialApp(
-      title: "Application",
+      debugShowCheckedModeBanner: false,
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
   );
+}
+Future<void> initServices() async { 
+  await Get.putAsync(() => SocketService().init());
+  await Get.putAsync(() => UserService().init());
 }
